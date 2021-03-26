14d9ba5d0acf370aaca1ee3decd18184b896babc
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-25 17:54:18 -0700

    Capitalize two words, first letter of each word.

--- JavaScriptPractice/JavaPractice.js
+++ JavaScriptPractice/JavaPractice.js
@@ -2,7 +2,7 @@
 
 //exerciseOne();
 //helloWorldone();
-helloWorld();
+//helloWorld();
 
 function helloWorld(){
     console.log("Hello world!");
@@ -32,8 +32,42 @@ function getString(){
     let wordToBeReversed = prompt("Enter your word to be reversed.");
     return wordToBeReversed;
 }
+function capitalizeTwoWordsTwo(){
+    let wordsToCapitalize = prompt("Enter the words you want capitalized. Limit two.");
+    wordsToCapitalize.toString();
+    let indexOfSpace = wordsToCapitalize.indexOf(" ");
+    let indexOfSecondWord = indexOfSpace+1;
+    let firstLetterOfString = wordsToCapitalize.charAt(0);
+    let upperCaseFirstLetter = firstLetterOfString.toUpperCase();
+    let secondLetter = wordsToCapitalize.charAt(indexOfSecondWord);
+    let capitalizedSecondLetter = secondLetter.toUpperCase();
+    wordsToCapitalize.replace(firstLetterOfString, upperCaseFirstLetter);
+    wordsToCapitalize.replace(secondLetter, capitalizedSecondLetter);
+    displayCapitalizedWords(wordsToCapitalize);
+}
+function capitalizeTwoWords(){
+    let wordsToCapitalize = prompt("Enter the words you want capitalized. Limit two.");
+    wordsToCapitalize.toString();
+    let indexOfSpace = wordsToCapitalize.indexOf(" ");
+    let indexOfSecondWord = indexOfSpace+1;
+    let firstLetterOfString = wordsToCapitalize.charAt(0);
+    let capitalFirstLet = firstLetterOfString.toUpperCase();
+    //wordsToCapitalize.replace(charAt(0), firstLetterOfString);
+    var newString = wordsToCapitalize.replace(firstLetterOfString, capitalFirstLet);
+    let secondLetterOfString = wordsToCapitalize.charAt(indexOfSecondWord);
+    let capitalSecondLetter = secondLetterOfString.toUpperCase();
+    var newStringTwo = newString.replace(secondLetterOfString, capitalSecondLetter);
+
+    displayCapitalizedWords(newStringTwo);
+
+}
+
+
 function displayStuff(stuff){
     document.getElementById("JSpractice").innerHTML = stuff;
+}
+function displayCapitalizedWords(words){
+    document.getElementById("capitalizedWords").innerHTML = words;
 }
 
 
